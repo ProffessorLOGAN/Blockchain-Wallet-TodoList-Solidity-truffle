cf453dceb4ec0e1abec367e24d39eaf0fa8dd4c1
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-08-21 13:43:29 +0530

    Update TaskContract
    Date- 21/8/22

--- backend/contracts/TaskContract.sol
+++ backend/contracts/TaskContract.sol
@@ -1,7 +1,46 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity 0.8.15;
 
 contract TaskContract {
-  constructor() public {
+ event AddTask(address recipient, uint taskId)
+ event DleteTask(uint taskId, bool isDeleted)
+
+struct Task{
+  uint id;
+  string taskText;
+  bool isDeleted;
+}
+
+Task[] private tasks;
+mapping(uint256 => address) taskToOwner;
+
+function addTask(string memory taskText, bool isDeleted)
+external{
+  unit taskId = tasks.lenght;
+  tasks.push( Task(taskId ,taskText , isDeleted) )
+  taskToOwner[taskId] = msg.sender;
+  emit AddTask(msg.sender , taskId);
+}
+
+function getMyTasks() external view returns (Task[] memory){
+  Task[] emorytemporary = new Task[](tasks.length);
+  unit counter = 0;
+
+
+for( unit i=0; i<tasks.lenght; i++){
+  if(taskToOwner[i] == msg.sender && tasks[i].isDeleted == false){
+    temporary[counter] = tasks[i];
+    counter++;
+
   }
+}
+
+Task[] memory result = new Task[](counter);
+for(uint i=0; i<counter; i++){
+  result[i] = temporary[i];
+}
+return result;
+}
+
+
 }
